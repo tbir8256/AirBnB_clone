5769ecfe1bd75f29ca62f7c1fff93b53938ad323
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-10-30 16:33:55 +0200

    Added storage method for task four and part of 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-
+import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Implements the BaseModel method."""
+    """Represents the BaseModel."""
 
-    def __init__(self, id, created_at, updated_at):
-        """Initialize a new BaseModel.
+    def __init__(self, *args, **kwargs):
+        """Initialize a BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
-        """Update updated_at"""
+        """Update updated_at."""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
@@ -30,5 +43,5 @@ class BaseModel:
         return bdict
 
     def __str__(self):
-        """Return the str representation of the BaseModel instance."""
+        """Return the str representation of the BaseModel."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
