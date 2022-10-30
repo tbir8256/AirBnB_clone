75ed3020b654c18def29f3e6bad665e815c0322c
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-10-30 18:45:24 +0200

    Fixed indent error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,6 +22,22 @@ class FileStorage:
         """Set __objects obj with obj_class_name.id"""
         objname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(objname, obj.id)] = obj
-
-    def save(self):
         
+    def save(self):
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
