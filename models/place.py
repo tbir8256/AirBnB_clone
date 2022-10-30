23a4adbc81466cbd9ee6cb23e35067ff0fafc461
TianaGovender <tianagovender1@gmail.com>
2022-10-30 17:40:21 +0000

    Task 7 complete

---
+++ models/place.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines city class"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Represent a city
+
+    Attributes:
+        state_id (str): Stae id
+        name (str): The name of the city
+    """
+
+    state_id = ""
+    name = ""
